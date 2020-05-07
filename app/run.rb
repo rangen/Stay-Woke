f6009396de6a306f308a82776707f47e69cab30d
Don Mallory <dmm333@gmail.com>
2020-05-07 10:28:04 -0700

    envy me    we have an fec MVP    we aint going nowhere so you should get to know me     and tisdale

--- app/run.rb
+++ app/run.rb
@@ -5,19 +5,16 @@ ActiveRecord::Base.establish_connection(
 class StayWokeCLI
     attr_accessor :user, :temp_user, :temp_active
     attr_reader :heart
-
     def initialize
         @prompt = TTY::Prompt.new
         @heart = @prompt.decorate(@prompt.symbols[:heart] + ' ', :bright_magenta)
         @temp_active = false
     end
-
     def welcome
       resp = @prompt.yes?('Welcome to Stay Woke! Is it your first time waking up?')
       resp || User.first.nil? ? new_user : find_user_name  #pretty genius || to stop login attempt to an empty db :)
       login
     end
-
     def new_user
         args = {}
         args[:first_name] = @prompt.ask("We've been expecting you. It's never too late to wake up and find out what's going on. Staying woke takes daily practice and mindfulness.  Let's help you with that by setting up a user profile. If I need to ask you a question, what is your first name?")
@@ -26,7 +23,6 @@ class StayWokeCLI
         seed_initial_data(args)
         puts "Let's fucking do this! You'll need a password to access the system."
     end
-
     def find_user_name #displays users in database  and user can select one to attempt to log in with
        names = User.all.map do |n|
             "#{n[:first_name]} #{n[:last_name]}"
@@ -41,7 +37,6 @@ class StayWokeCLI
         # end
         # @user = @prompt.select("Select a user:", Hash[choices]) 
     end
-
     def login
         #checks the newly loaded @user variable for a password.  if new user, create and exit w/ true.  if password, ask for password and rturn
         if @user.password.nil?
@@ -57,11 +52,10 @@ class StayWokeCLI
          end
         #check and branch to new_user or set instance variables
     end
-
     def main_menu
+        @temp_user.destroy if @temp_user
         @temp_active = false #reset temp_user when returning to main menu
         @temp_user = nil
-
         choices = {"Show Information for My District" => 2,
              "Show Information for Another District" => 3,
              "Settings" => 4, "Exit" => 1
@@ -78,18 +72,15 @@ class StayWokeCLI
             settings
        end
     end
-
     def exit
         puts "Enjoy your slumber. Come back when you're ready to be woke."  #colorify this shit Tisdale!
     end
-
     def retrieve_other_address_as_user_obj  #give user a chance to enter a new address to see data for; give option for random address with 'random'
         other_address = @prompt.ask("Enter a US address or 'random' to retrieve district info:", default: "1520 Marion Lincoln Park MI")
         @temp_active = true
         seed_initial_data(address: other_address)
         @temp_user
     end
-
     def info_for_district(user)
        choices = {user.politicians[0].name => 1, user.politicians[1].name => 2, user.politicians[2].name => 3, "Return to Main Menu" => 4}
        resp = @prompt.select("Please choose one of the following:", choices, cycle: true)
@@ -104,7 +95,6 @@ class StayWokeCLI
         main_menu
        end
     end
-
     def show_politician_info(pol)   #
         choices = {pol.domain=>1, pol.title=>2, "@" + pol.twitter=>3, "Return to District"=>4}
         resp = @prompt.select("Showing Info for #{pol.name}", choices)
@@ -114,7 +104,6 @@ class StayWokeCLI
             info_for_district(result)
         end
     end
-
     def settings
         choices = {"Delete Data" => 1,
             "Delete Account" => 2,
@@ -139,6 +128,9 @@ class StayWokeCLI
     def delete_data
     end
     def delete_account
+        @user.destroy
+        puts "Account deleted. Don't feed the hand that bites you." 
+        welcome
     end
     def change_address
     end
@@ -147,9 +139,7 @@ class StayWokeCLI
     def exit
        puts "Enjoy your slumber. Come back when you're ready to be woke."
     end
-
     private
-    
     def seed_initial_data(args)
         puts "Building your initial dataset...."   #COLORIFY TISDALE
         hold_me = User.create(args)
