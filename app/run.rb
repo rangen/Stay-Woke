dfe6bce89650d964c0f519109c3ac52cf3861c94
Don Mallory <dmm333@gmail.com>
2020-05-07 00:57:00 -0700

    menus functioning

--- app/run.rb
+++ app/run.rb
@@ -15,7 +15,6 @@ class StayWokeCLI
       resp ? new_user : find_user_name
       login
     end
-
     def new_user
         args = {}
         resp = @prompt.ask("We've been expecting you. It's never too late to wake up and find out what's going on. Staying woke takes daily practice and mindfulness.  Let's help you with that by setting up a user profile. If I need to ask you a question, what is your first name?")
@@ -25,7 +24,6 @@ class StayWokeCLI
         resp = @prompt.ask("In order to properly assist you #{args[:first_name]}, your address will be required. This information will be kept completely private unless you try to break our code.")
         args[:address] = resp
         @user = User.create(args)
-
         #Populate initial info for the new users' politicians
         @user.find_my_servants
         @user.politicians.pluck(:candidate_id).each {|can| GetCandidateInfo.new(can).seek}
@@ -40,7 +38,6 @@ class StayWokeCLI
         resp = @prompt.select("Select a user:", names)
         first, last = resp.split
         @user = User.find_by(first_name: first, last_name: last)# could use new variable of name = first + last
-    
         # choices = []
         # User.all.each do |n|
         #     choices << "#{n[:first_name]} #{n[:last_name]}"
@@ -48,7 +45,6 @@ class StayWokeCLI
         # end
         # @user = @prompt.select("Select a user:", Hash[choices]) 
     end
-
     def login
         #checks the newly loaded @user variable for a password.  if new user, create and exit w/ true.  if password, ask for password and rturn
         if @user.password.nil?
@@ -64,7 +60,6 @@ class StayWokeCLI
          end
         #check and branch to new_user or set instance variables
     end
-
     def main_menu
         choices = {"Exit" => 1,
              "Show information for my district" => 2,
@@ -82,12 +77,10 @@ class StayWokeCLI
         when 4
         settings
        end
-
     end
     def exit
         puts "Enjoy your slumber. Come back when you're ready to be woke."
     end
-
     def retrieve_other_address_as_user_obj  #give user a chance to enter a new address to see data for; give option for random address with 'random'
         other_address = @prompt.ask("Enter a US address or 'random' to retrieve district info:", default: "1520 Marion Lincoln Park MI")
         @temp_user = User.create(address: other_address)
@@ -95,10 +88,9 @@ class StayWokeCLI
         @temp_active = true
         @temp_user
     end
-
     def info_for_district(user)
        #@address = @user.address
-       choices = {user.politician[0] => 1, user.politician[1] => 2, user.politician[2] => 3, "Return to Main Menu" => 4}
+       choices = {user.politicians[0].name => 1, user.politicians[1].name => 2, user.politicians[2].name => 3, "Return to Main Menu" => 4}
        resp = @prompt.select("Please choose one of the following:", choices, cycle: true)
        case resp
        when 1
@@ -111,13 +103,15 @@ class StayWokeCLI
         main_menu
        end
     end
-
     def show_politician_info(pol)   #
         choices = {pol.domain=>1, pol.title=>2, "@" + pol.twitter=>3, "Return to District"=>4}
-        resp = @prompt.select("Showing Info for #{pol.name}")
-    
+        resp = @prompt.select("Showing Info for #{pol.name}", choices)
+        case resp
+        when 4
+            result = @temp_active ? @temp_user : @user
+            info_for_district(result)
+        end
     end
-
     def settings
         choices = {"Delete Data" => 1,
             "Delete Account" => 2,
@@ -126,38 +120,32 @@ class StayWokeCLI
             "Return to Main Menu" => 5
         }
         resp = @prompt.select("Please choose from one of the following:", choices, cycle: true)
-            # case resp
-            
-            # end
-
+            case resp
+            when 1
+                delete_data
+            when 2
+                delete_account
+            when 3
+                change_address
+            when 4 
+                change_password
+            when 5
+                main_menu
+            end
     end
-
     def delete_data
-
     end
-
     def delete_account
-
     end
-
     def change_address
-
     end
-
     def change_password
-        
     end
-
     def exit
        puts "Enjoy your slumber. Come back when you're ready to be woke."
     end
-
-
-
 end
-
 #run.rb will start here  (maybe some require_relative statements idfk but just the below codes)
 sess = StayWokeCLI.new
 puts sess.welcome
-
 sess.main_menu
